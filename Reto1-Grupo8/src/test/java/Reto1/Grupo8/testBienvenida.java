6b98007c3559a0805344b173335bc54263511cfe
in2daw <in2daw@elorrieta.int>
2020-11-09 19:28:41 +0100

    Hecho Ventana Bienvenida

---
+++ Reto1-Grupo8/src/test/java/Reto1/Grupo8/testBienvenida.java
@@ -0,0 +1,14 @@
+package Reto1.Grupo8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testBienvenida {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
