c9195679f0f25dcd3f4132c10bce43eb6e9c5200
MarkelCuesta <markel.cuestaar@elorrieta-errekamari.com>
2020-11-20 04:23:48 +0100

    Arreglando bug timer

--- Reto1-Grupo8/src/main/java/controlador/Funciones_Bienvenida.java
+++ Reto1-Grupo8/src/main/java/controlador/Funciones_Bienvenida.java
@@ -15,8 +15,14 @@ public class Funciones_Bienvenida {
 	 * @wbp.parser.entryPoint
 	 */
 	public static void pasar_ventana(JFrame ventana) {
+		int segundos=0;
+		if(ventana == Frame_Bienvenida.frmBienvenida) 
+			segundos = 3;
+		else if(ventana == Frame_Final.frame_final) 
+			segundos = 2;
+		
 		//temporizador para cambiar de ventana
-				tiempo = new Timer(2000,null);
+				tiempo = new Timer(segundos*1000,null);
 				tiempo.start();
 				tiempo.addActionListener(new java.awt.event.ActionListener() {
 					
@@ -33,16 +39,20 @@ public class Funciones_Bienvenida {
 			if(ventana == Frame_Bienvenida.frmBienvenida) {
 				Frame_Login login = new Frame_Login();
 				Frame_Login.setFrame(login);
+				tiempo.stop();
+				parar_timer = false;
 				login.setVisible(true);
 				Frame_Bienvenida.getFrmBienvenida().dispose();
 			}
 			else if(ventana == Frame_Final.frame_final) {
 				Frame_Final.frame_final.dispose();
-				Frame_Bienvenida.frmBienvenida.setVisible(true);
+				tiempo.stop();
+				parar_timer = false;
+				new Frame_Bienvenida().frmBienvenida.setVisible(true);
 			}
 			
-			tiempo.stop();
-			parar_timer = false;
+//			tiempo.stop();
+//			parar_timer = false;
 		}
 
 }
