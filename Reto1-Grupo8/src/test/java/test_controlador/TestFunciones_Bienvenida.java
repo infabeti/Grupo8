5819bd6f6d26f148a9c65de79844a06e1de5fe31
MarkelCuesta <markel.cuestaar@elorrieta-errekamari.com>
2020-11-22 17:20:36 +0100

    corrección error testeo bienvenida, faltaba un parámetro

--- Reto1-Grupo8/src/test/java/test_controlador/TestFunciones_Bienvenida.java
+++ Reto1-Grupo8/src/test/java/test_controlador/TestFunciones_Bienvenida.java
@@ -3,6 +3,7 @@ package test_controlador;
 import org.junit.Test;
 
 import controlador.Funciones_Bienvenida;
+import vista.Frame_Bienvenida;
 
 class TestFunciones_Bienvenida {
 	
@@ -11,7 +12,7 @@ class TestFunciones_Bienvenida {
 	@Test
 	public void testBienvenida() {
 		resultado = true;
-		Funciones_Bienvenida.pasar_ventana();
+		Funciones_Bienvenida.pasar_ventana(Frame_Bienvenida.frmBienvenida);
 		
 		org.junit.Assert.assertEquals(resultado, Funciones_Bienvenida.iniciar_login);
 	}
