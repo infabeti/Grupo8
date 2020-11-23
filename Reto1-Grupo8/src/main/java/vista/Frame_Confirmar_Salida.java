c5b1f8375d27dca5529f1ac68c845e5fdda0bf09
alberto-garcia-lo <alberto.garcialo@elorrieta-errekamari.com>
2020-11-23 01:11:28 +0100

    diseño bienvenida,generos,confirmar salida,resumen

--- Reto1-Grupo8/src/main/java/vista/Frame_Confirmar_Salida.java
+++ Reto1-Grupo8/src/main/java/vista/Frame_Confirmar_Salida.java
@@ -13,6 +13,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Frame_Confirmar_Salida {
 
@@ -46,10 +47,14 @@ public class Frame_Confirmar_Salida {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 140, 0));
 		frame.setBounds(100, 100, 269, 213);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton btn_confirmar = new JButton("Aceptar");
+		btn_confirmar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btn_confirmar.setBorderPainted(false);
+		btn_confirmar.setBackground(new Color(255, 255, 255));
 		btn_confirmar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Frame_Generos.frame_generos.dispose();
@@ -64,6 +69,8 @@ public class Frame_Confirmar_Salida {
 		});
 		
 		JButton btn_cancelar = new JButton("Cancelar");
+		btn_cancelar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btn_cancelar.setBorderPainted(false);
 		btn_cancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				frame.dispose();
@@ -71,16 +78,15 @@ public class Frame_Confirmar_Salida {
 		});
 		
 		JLabel lbl_confirmar_salir = new JLabel("\u00BFEst\u00E1 seguro que desea salir?");
-		lbl_confirmar_salir.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lbl_confirmar_salir.setForeground(new Color(255, 255, 255));
+		lbl_confirmar_salir.setFont(new Font("Tahoma", Font.BOLD, 14));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(27, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(9)
-							.addComponent(lbl_confirmar_salir, GroupLayout.PREFERRED_SIZE, 178, GroupLayout.PREFERRED_SIZE))
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(21, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lbl_confirmar_salir, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(btn_confirmar, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
 							.addGap(29)
@@ -90,9 +96,9 @@ public class Frame_Confirmar_Salida {
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(43)
+					.addGap(42)
 					.addComponent(lbl_confirmar_salir, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-					.addGap(43)
+					.addGap(44)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(btn_confirmar)
 						.addComponent(btn_cancelar))
